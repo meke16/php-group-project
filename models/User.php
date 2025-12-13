9c4f82a292e949a61ddbd9a1857945f51b9a5ecf
Firaol Abera <firaol991@gmail.com>
2025-12-13 14:07:31 +0300

    removed unwanted methods

--- models/User.php
+++ models/User.php
@@ -23,30 +23,10 @@ class User {
         return false;
     }
 
-    // Get user by ID
-    public function find($id) {
-        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
     public static function findByUsername($username) {
         global $pdo;
         $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
         $stmt->execute([$username]);
         return $stmt->fetch();
-    }
-    // List all users
-    public function all() {
-        $stmt = $this->pdo->query("SELECT * FROM users");
-        $stmt->execute();
-        return $stmt->fetchAll();
-    }
-
-    // Create a new user (for seeding)
-    public function create($username, $password, $role) {
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        $stmt = $this->pdo->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
-        return $stmt->execute([$username, $hash, $role]);
     }
 }
